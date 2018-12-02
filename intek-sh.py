1c3331462224f9d1827b0a7e9dc3df0a2055c7af
mbach <you@example.com>
2018-12-02 14:33:41 +0700

    Modify Logical Operator feature to fit with the Minish
    Have to implement with Exit function since this feature is based on the exit_code of the executed command
    Introduce self.raw_input in the function, modify self.user_input and self.exit_code to return the expected result using && and ||

--- intek-sh.py
+++ intek-sh.py
@@ -17,25 +17,89 @@ class Shell:
     def __init__(self):
         # list of built-in features
         self.builtins = ['exit', 'printenv', 'export', 'unset', 'cd']
+        self.exit_code = 0
         # run the REPL
         loop = True
         while loop:
             # get inputs from user as a list
             self.user_input = split(input('intek-sh$ '), posix=True)
+            self.handle_exit_code()
             try:
-                command = self.user_input[0]
-                # check if command is a built-in
-                if command in self.builtins:
-                    self.do_builtin(command)
-                # if command is not a built-in
+                # handle && and || separately
+                if '&&' in self.user_input or '||' in self.user_input:
+                    self.raw_input = ' '.join(self.user_input)
+                    self.logical_operator(self.raw_input)
                 else:
-                    self.do_external(command)
+                    command = self.user_input[0]
+                    # check if command is a built-in
+                    if command in self.builtins:
+                        self.do_builtin(command)
+                    # if command is not a built-in
+                    else:
+                        self.do_external(command)
             # catch EOFError when no input is prompted in
             except EOFError:
                 break
             # catch IndexError when nothing is input in (empty input list)
             except IndexError:
                 pass
+
+    # convert $? to exit code
+    def handle_exit_code(self):
+        if '$?' in self.user_input:
+            pos = self.user_input.index('$?')
+            self.user_input[pos] = str(self.exit_code)
+
+    # logical operator handling feature
+    def logical_operator(self, inputs):
+        # base case for recursion - if no logical operator in inputs
+        if '&&' not in inputs and '||' not in inputs:
+            self.user_input = split(inputs, posix=True)
+            command = self.user_input[0]
+            if command in self.builtins:
+                self.do_builtin(command)
+            else:
+                self.do_external(command)
+            return self.exit_code
+        # if logical operator in inputs
+        else:
+            # if both in inputs
+            if '&&' in inputs and '||' in inputs:
+                pos1 = inputs.index('&&', 1)
+                pos2 = inputs.index('||', 1)
+                # if the first logical operator is &&
+                if pos1 < pos2:
+                    left = inputs[:pos1 - 1]
+                    right = inputs[pos1 + 3:]
+                    expected = True
+                # if the first logical operator is ||
+                else:
+                    left = inputs[:pos2 - 1]
+                    right = inputs[pos2 + 3:]
+                    expected = False
+            # if only && in inputs
+            elif '&&' in inputs and '||' not in inputs:
+                pos1 = inputs.index('&&', 1)
+                left = inputs[:pos1 - 1]
+                right = inputs[pos1 + 3:]
+                expected = True
+            # if only || in inputs
+            elif '||' in inputs and '&&' not in inputs:
+                pos2 = inputs.index('||', 1)
+                left = inputs[:pos2 - 1]
+                right = inputs[pos2 + 3:]
+                expected = False
+            self.user_input = split(left, posix=True)
+            command = self.user_input[0]
+            if command in self.builtins:
+                self.do_builtin(command)
+            else:
+                self.do_external(command)
+            # if exit_code is not what the condition expects then return
+            if not self.exit_code != expected:
+                return self.exit_code
+            else:
+                self.logical_operator(right)
 
     # execute the function with the same name as the command
     def do_builtin(self, command):
@@ -44,12 +108,20 @@ class Shell:
     # exit feature
     def exit(self):
         print('exit')
-        if len(self.user_input) is 2 and not self.user_input[1].isdigit():
-            print('intek-sh: exit:')
-        exit()
+        if len(self.user_input) > 1 and not self.user_input[1].isdigit():
+            print('intek-sh: exit: ' + self.user_input[1] +
+                  ': numeric argument required')
+            exit(2)
+        elif len(self.user_input) > 2:
+            self.exit_code = 1
+            print('intek-sh: exit: too many arguments')
+        else:
+            if len(self.user_input) > 1:
+                self.exit_code = int(self.user_input[1])
+            exit(self.exit_code)
 
     # printenv feature
-    def printenv(self):
+    def printenv(self, flag=False):
         # if no argument is provided
         if len(self.user_input) is 1:
             for key in environ:
@@ -61,10 +133,15 @@ class Shell:
                     print(environ[key])
                 # catch KeyError when an argument not exists in environ
                 except KeyError:
+                    flag = True
                     pass
+        if not flag:
+            self.exit_code = 0
+        elif flag:
+            self.exit_code = 1
 
     # export feature
-    def export(self):
+    def export(self, flag=False):
         for item in self.user_input[1:]:
             if '=' in item:
                 items = item.split('=', 1)
@@ -73,6 +150,7 @@ class Shell:
                 else:
                     print('intek-sh: export:' +
                           ' `%s\': not a valid identifier' % (item))
+                    flag = True
             else:
                 # handle the case ''; '   '; 'b    a'
                 item_strip = item.strip().split(' ')
@@ -81,9 +159,14 @@ class Shell:
                 else:
                     print('intek-sh: export:' +
                           ' `%s\': not a valid identifier' % (item))
+                    flag = True
+        if not flag:
+            self.exit_code = 0
+        elif flag:
+            self.exit_code = 1
 
     # unset feature
-    def unset(self):
+    def unset(self, flag=False):
         for key in self.user_input[1:]:
             try:
                 # handle the case ''; '   '; 'b    a'
@@ -97,12 +180,19 @@ class Shell:
                 else:
                     print('intek-sh: unset:' +
                           ' `%s\': not a valid identifier' % (key))
+                    flag = True
+            # catch if no execute permission on key
             except OSError:
                 print('intek-sh: unset:' +
                       ' `%s\': not a valid identifier' % (key))
+                flag = True
+        if not flag:
+            self.exit_code = 0
+        elif flag:
+            self.exit_code = 1
 
     # cd feature
-    def cd(self):
+    def cd(self, flag=False):
         try:
             if len(self.user_input) is 1:
                 dir_path = environ['HOME']
@@ -116,14 +206,21 @@ class Shell:
         # catch when variable HOME doesn't have value
         except KeyError:
             print('intek-sh: cd: HOME not set')
+            flag = True
         # catch when destination not exist
         except FileNotFoundError:
             print('intek-sh: cd:' +
                   ' %s: No such file or directory' % (self.user_input[1]))
+            flag = True
         # catch when destination is not a directory
         except NotADirectoryError:
             print('intek-sh: cd:' +
                   ' %s: Not a directory' % (self.user_input[1]))
+            flag = True
+        if not flag:
+            self.exit_code = 0
+        elif flag:
+            self.exit_code = 1
 
     # execute external command
     def do_external(self, command):
@@ -140,17 +237,21 @@ class Shell:
             # catch if only ./ is prompted in
             if command == './':
                 print('intek-sh: ./: Is a directory')
+                self.exit_code = 126
             else:
                 # run the file
-                run(self.user_input)
+                self.exit_code = run(self.user_input).returncode
         # catch if no execute permission on the file
         except PermissionError:
             print('intek-sh: %s: Permission denied' % (command))
+            self.exit_code = 126
         # catch if the file doesn't exist
         except FileNotFoundError:
             print('intek-sh: %s: No such file or directory' % (command))
+            self.exit_code = 127
         # catch if file is not an executable file
         except OSError:
+            self.exit_code = 0
             pass
 
     # run the external binaries
@@ -160,16 +261,19 @@ class Shell:
             paths = environ['PATH'].split(':')
             # check if the command is in paths
             if (exists(path + '/' + command) for path in paths):
-                run(self.user_input)
+                self.exit_code = run(self.user_input).returncode
         # catch if the command doesn't exist
         except FileNotFoundError:
             print('intek-sh: %s: command not found' % (command))
+            self.exit_code = 127
         # catch if PATH variable doesn't exist
         except KeyError:
             print('intek-sh: %s: No such file or directory' % (command))
+            self.exit_code = 127
         # catch if no execute permission on the command
         except PermissionError:
             print('intek-sh: %s: Permission denied' % (command))
+            self.exit_code = 126
 
 
 # Run the Shell
--- logical_operator.py
+++ logical_operator.py
@@ -1,36 +1,53 @@
 from subprocess import run
 
 
-def logical_operator(inputs):
-
+# logical operator handling feature
+def logical_operator(self, inputs):
+    # base case for recursion - if no logical operator in inputs
     if '&&' not in inputs and '||' not in inputs:
-        exit_code = run(inputs.split()).returncode
-        print(exit_code)
-        return exit_code
+        self.user_input = split(inputs, posix=True)
+        command = self.user_input[0]
+        if command in self.builtins:
+            self.do_builtin(command)
+        else:
+            self.do_external(command)
+        return self.exit_code
+    # if logical operator in inputs
     else:
+        # if both in inputs
         if '&&' in inputs and '||' in inputs:
             pos1 = inputs.index('&&', 1)
             pos2 = inputs.index('||', 1)
+            # if the first logical operator is &&
             if pos1 < pos2:
                 left = inputs[:pos1 - 1]
                 right = inputs[pos1 + 3:]
                 expected = True
+            # if the first logical operator is ||
             else:
                 left = inputs[:pos2 - 1]
                 right = inputs[pos2 + 3:]
                 expected = False
+        # if only && in inputs
         elif '&&' in inputs and '||' not in inputs:
             pos1 = inputs.index('&&', 1)
             left = inputs[:pos1 - 1]
             right = inputs[pos1 + 3:]
             expected = True
+        # if only || in inputs
         elif '||' in inputs and '&&' not in inputs:
             pos2 = inputs.index('||', 1)
             left = inputs[:pos2 - 1]
             right = inputs[pos2 + 3:]
             expected = False
-        exit_code = run(left.split()).returncode
-        if not exit_code != expected:
-            return exit_code
+        self.user_input = split(left, posix=True)
+        command = self.user_input[0]
+        if command in self.builtins:
+            self.do_builtin(command)
         else:
-            logical_operator(right)
+            self.do_external(command)
+        # if exit_code is not what the condition expects then return
+        if not self.exit_code != expected:
+            return self.exit_code
+        else:
+            self.logical_operator(right)
