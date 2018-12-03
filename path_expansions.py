1d3011c585092ee4ca0d64308e707d99569ed3f4
AN <phamhaian.ab@gmail.com>
2018-12-03 15:00:23 +0700

    modified path_expansion and intek-sh
    Almost finish debuging:
    - handle input inside path_expans function
    - include tilde_expans and parameter_expans
    - done $HOME+378234(res: +378234), $HOME890sb(res: ''), $asdhj(res: '') case

--- path_expansions.py
+++ path_expansions.py
@@ -2,15 +2,6 @@ from os.path import expanduser, expandvars
 from os import getenv
 
 
-# def get_dirs_stack(expanded):
-#     if expanded[1:].isdigit():
-#         pass
-#     elif expanded.startswith('~+') or expanded.startswith('~-'):
-#         if expanded[2:].isdigit():
-#             pass
-#     return expanded
-
-
 def tilde_expans(path):
     expanded = expanduser(path)
     if expanded.startswith('~'):
@@ -24,16 +15,33 @@ def tilde_expans(path):
 
 
 def para_expans(path):
-    return expandvars(path)
+    var = expandvars(path)
+    if '$' in var:
+        i = var.index('$') + 1
+        j = i
+        while j < len(var) and var[j].isalnum() and var[j] != ' ':
+            j+=1
+        return var[:i-1] + var[j:]
+    else:
+        return var
 
 
-def path_expans(path):
+def path_expans_one(path):
     if path.startswith('~'):
         return tilde_expans(path)
-    elif path.startswith('$'):
+    elif '$' in path:
         return para_expans(path)
 
 
+def path_expans(inp):
+    path_expaned = []
+    for arg in inp:
+        if '$' in arg or '~' in arg:
+            path_expaned.append(path_expans_one(arg))
+        else:
+            path_expaned.append(arg)
+    return path_expaned
+
 
 if __name__ == '__main__':
-    print(path_expans('~-/'))
+    print(path_expans('~/*'))
