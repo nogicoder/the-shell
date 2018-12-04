f60dc19f31969f993db981f3fd06c77883212fcd
nson <saika@DESKTOP-D923QRS.localdomain>
2018-12-04 14:12:24 +0700

    Added 2 builtins and rewrite structure + Added MyHistory module + Added 'history' builtin + Added '!' - do_exclamation builtin + Rewrite while-loop: 	- Added do_command() method 	- Change handle_input() (now can provide 2 ways of input)

---
+++ MyHistory.py
@@ -0,0 +1,37 @@
+from shlex import split
+
+def write_history(inp):
+    if inp is not '':
+        with open("history.txt", 'a+') as history_file:
+            # get line number
+            history_file.seek(0)
+            num_line = sum(1 for line in history_file) + 1
+            # get content of history file
+            history_file.seek(0)
+            content = history_file.readlines()
+            # check if history file is not empty
+            if num_line > 1:
+                should_write = True
+                last_line = content[-1].split('\t')
+                last_inp = last_line[1].strip()
+                # input duplicate, should not write
+                if inp == last_inp:
+                    should_write = False
+            # if history file is empty
+            else:
+                should_write = True
+            # write to history file
+            if should_write:
+                history_file.write(str(num_line) + '\t' + inp + '\n')
+
+
+def print_newest_history(n):
+    try:
+        with open('history.txt', 'r') as history_file:
+            # get content of history file
+            content = history_file.readlines()
+            # print n newest lines 
+            for line in content[-int(n):]:
+                print('  ' + line.strip())
+    except ValueError:
+        print('intek-sh: history: %s: numeric argument required' % (n))
--- intek-sh.py
+++ intek-sh.py
@@ -9,6 +9,7 @@ from shlex import split
 from subprocess import run
 from string import ascii_lowercase
 from string import ascii_uppercase
+from MyHistory import write_history, print_newest_history
 
 
 '''----------------------Create a Shell Object-----------------------------'''
@@ -19,31 +20,38 @@ class Shell:
     def __init__(self):
         self.ascii_list = ascii_lowercase + ascii_uppercase
         # list of built-in features
-        self.builtins = ['exit', 'printenv', 'export', 'unset', 'cd']
+        self.builtins = ('exit', 'printenv', 'export', 'unset', 'cd', 'history')
         # run the REPL
         loop = True
         while loop:
-            # get inputs from user as a list
-            self.user_input = self.handle_input()
-            try:
-                command = self.user_input[0]
-                # check if command is a built-in
-                if command in self.builtins:
-                    self.do_builtin(command)
-                # if command is not a built-in
-                else:
-                    self.do_external(command)
-            # catch EOFError when no input is prompted in
-            except EOFError:
-                break
-            # catch IndexError when nothing is input in (empty input list)
-            except IndexError:
-                pass
+
+            self.handle_input()
+            write_history(self.inp)
+            self.do_command(self.user_input)
 
     # Handling input to match each feature's requirement
     def handle_input(self):
-        user_input = split(input('intek-sh$ '), posix=True)
-        return user_input
+        self.inp = input('intek-sh$ ')
+        self.user_input = split(self.inp, posix=True)
+
+    def do_command(self, user_input):
+        try:
+            command = user_input[0]
+            # check if command is a built-in
+            if command in self.builtins:
+                self.do_builtin(command)
+            # check if command is '!*'
+            elif command.startswith('!'):
+                self.do_exclamation(command)
+            # if command is not a built-in
+            else:
+                self.do_external(command, user_input)
+        # catch EOFError when no input is prompted in
+        except EOFError:
+            pass
+        # catch IndexError when nothing is input in (empty input list)
+        except IndexError:
+            pass
 
     # execute the function with the same name as the command
     def do_builtin(self, command):
@@ -155,25 +163,58 @@ class Shell:
         except NotADirectoryError:
             print('intek-sh: cd:' +
                   ' %s: Not a directory' % (self.user_input[1]))
+    
+    def history(self):
+        # with only 'history' command
+        if len(self.user_input) is 1:
+            with open('history.txt', 'r') as history_file:
+                for line in history_file:
+                    print('  ' + line.strip())
+        # 'history + options' command
+        else:
+            # clear option 'history -c'
+            if '-c' in self.user_input[1:]:
+                open('history.txt', "w").close()
+            # display [n] newest history 'history [n]'
+            else:
+                # number of last lines need to print
+                n = self.user_input[1]
+                # function to print
+                print_newest_history(n)
+    # '!' command, ralating to history
+    def do_exclamation(self, command):
+        try:
+            numline = int(command[1:])
+            with open('history.txt','r') as history_file:
+                content = history_file.readlines()
+                line_content = content[numline - 1].split('\t')[1].strip()
+                line_contents = split(line_content, posix=True)
+
+                print(line_content)
+                write_history(line_content)
+                self.do_command(line_contents)
+                
+        except (IndexError, ValueError):
+            print('intek-sh: %s: event not found' % (command))
 
     # execute external command
-    def do_external(self, command):
+    def do_external(self, command, user_input):
         # if command is an executable file
         if command.startswith('./'):
-            self.run_file(command)
+            self.run_file(command, user_input)
         # if command is not an executable file
         else:
-            self.run_binary(command)
+            self.run_binary(command, user_input)
 
     # run the executable file
-    def run_file(self, command):
+    def run_file(self, command, user_input):
         try:
             # catch if only ./ is prompted in
             if command == './':
                 print('intek-sh: ./: Is a directory')
             else:
                 # run the file
-                run(self.user_input)
+                run(user_input)
         # catch if no execute permission on the file
         except PermissionError:
             print('intek-sh: %s: Permission denied' % (command))
@@ -185,13 +226,13 @@ class Shell:
             pass
 
     # run the external binaries
-    def run_binary(self, command):
+    def run_binary(self, command, user_input):
         try:
             # get paths to external binaries indicated by variable PATH
             paths = environ['PATH'].split(':')
             # check if the command is in paths
             if command and (exists(path + '/' + command) for path in paths):
-                run(self.user_input)
+                run(user_input)
         # catch if the command doesn't exist
         except FileNotFoundError:
             print('intek-sh: %s: command not found' % (command))
@@ -201,7 +242,6 @@ class Shell:
         # catch if no execute permission on the command
         except PermissionError:
             print('intek-sh: %s: Permission denied' % (command))
-
 
 # Run the Shell
 if __name__ == '__main__':
