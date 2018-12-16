1e07060f7e0cdba7796b82761b92f9aa28ad0c54
mbach <bachm.biz@gmail.com>
2018-12-16 18:55:35 +0700

    Fix bug in exit_handling code and changing intek-sh.py

--- quoting.py
+++ quoting.py
@@ -1,4 +1,7 @@
 from shlex import split
+from path_expansions import path_expans
+from globbing import globbing
+from exit_code import handle_exit_code
 
 
 def process_new_line(new_line, user_input, quote):
@@ -54,3 +57,66 @@ def adding_backslash(raw_input, user_input=''):
                     user_input = process_new_line(new_line, user_input, quote)
             i += 1
     return user_input
+
+
+def handle_single_quote(item, pos1, pos2, exit_code):
+    if pos1 < pos2:
+        temp_left = handle_exit_code(item[:pos1], exit_code)
+        left = " ".join(path_expans(split('"' + temp_left + '"', posix=True)))
+        mid = item[pos1 + 1:pos2]
+        temp_right = handle_exit_code(item[pos2 + 1:], exit_code)
+        right = " ".join(path_expans(split('"' + temp_right + '"', posix=True)))
+    elif pos1 > pos2:
+        left = item[:pos2]
+        temp_mid = handle_exit_code(item[pos2 + 1:pos1], exit_code)
+        mid = handle_exit_code(" ".join(path_expans(split('"' + temp_mid + '"', posix=True))), exit_code)
+        right = item[pos1 + 1:]
+
+    if "'" in mid:
+        pos1 = mid.index("'", 0)
+        pos2 = len(mid) - mid[::-1].index("'") - 1
+        mid = handle_single_quote(mid, pos2, pos1, exit_code)
+    if left and right:
+        item = left + mid + right
+    elif left and not right:
+        item = left + mid
+    elif right and not left:
+        item = mid + right
+    else:
+        item = mid
+    return item
+
+
+def handle_quotes(user_input, exit_code):
+    result = []
+    for item in user_input:
+        if '"' in item and "'" in item:
+            pos1 = item.index("'", 0)
+            pos2 = item.index('"', 0)
+            pos3 = len(item) - item[::-1].index("'") - 1
+            pos4 = len(item) - item[::-1].index('"') - 1
+            # if single quote open (and close)
+            if pos1 < pos2:
+                item = handle_single_quote(item, pos1, pos3, exit_code)
+            # if double quote open (and close)
+            else:
+                item = handle_exit_code(item, exit_code)
+                item = " ".join(path_expans(split(item, posix=True)))
+        elif '"' in item and "'" not in item:
+            item = handle_exit_code(item, exit_code)
+            item = " ".join(path_expans(split(item, posix=True)))
+        elif "'" in item and '"' not in item:
+            pos1 = item.index("'", 0)
+            pos2 = len(item) - item[::-1].index("'") - 1
+            item = handle_single_quote(item, pos1, pos2, exit_code)
+        else:
+            item = handle_exit_code(item, exit_code)
+            if "'" in item:
+                item_list = split('"' + item + '"', posix=True)
+            else:
+                item_list = split("'" + item + "'", posix=True)
+            item = " ".join(path_expans(item_list))
+        item_list = split("'" + item + "'", posix=True)
+        item = " ".join(globbing(item_list))
+        result.append(item)
+    return result
